d8166b80bcf8587e2e1f4751c1f80ea50734a8e5
charlieb <charlie.burrows@gmail.com>
2019-04-30 12:21:39 -0400

    Initial empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to constraints
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/constraints/core_test.clj
@@ -0,0 +1,7 @@
+(ns constraints.core-test
+  (:require [clojure.test :refer :all]
+            [constraints.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
