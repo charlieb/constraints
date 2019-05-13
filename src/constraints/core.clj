bd80b170ef2ec21c2dd224af3ac963b89effe31c
charlieb <charlie.burrows@gmail.com>
2019-05-13 13:21:14 -0400

    Removed circle code from constraints. Added protocol for constraints implemented by Point type

--- src/constraints/core.clj
+++ src/constraints/core.clj
@@ -9,6 +9,21 @@
   (norm [this])
   )
 
+(defprotocol Constraints
+  (dist< [this p distance] [this p distance correction-factor]) ;"Ensure distance is less than the value specified"
+  (dist> [this p distance] [this p distance correction-factor]) ;"Ensure distance is greater than the value specified"
+  (dist= [this p distance] [this p distance correction-factor]) ;"Ensure distance is less than the value specified"
+  )
+
+(defn- set-dist [p1 p2 d condition correction-factor]
+  (let [delta (sub p2 p1)
+        dist (mag delta)
+        dir (norm delta)]
+    (if (condition dist d)
+      (sub p1 (mul (mul dir (- d dist))
+                   correction-factor))
+      p1)))
+
 (deftype Point [x y] 
   V2
   (add [p1 p2] (Point. (+ (.x p1) (.x p2)) 
@@ -25,50 +40,18 @@
                          (* (.y p) (.y p)))))
 
   (norm [p] (div p (mag p)))
-  )
+
+  Constraints
+  (dist< [p1 p2 dst] (dist< p1 p2 dst 1.0))
+  (dist< [p1 p2 dst factor] (set-dist p1 p2 dst > factor))
+
+  (dist> [p1 p2 dst] (dist> p1 p2 dst 1.0))
+  (dist> [p1 p2 dst factor] (set-dist p1 p2 dst < factor))
+
+  (dist=[p1 p2 dst] (dist< p1 p2 dst 1.0))
+  (dist= [p1 p2 dst factor] (set-dist p1 p2 dst (constantly true) factor)))
 
 
-(defn set-dist [p1 p2 d condition correction-factor]
-  (let [delta (sub p2 p1)
-        dist (mag delta)
-        dir (norm delta)]
-    (if (condition dist d)
-      (sub p1 (mul (mul dir (- d dist))
-                   correction-factor))
-      p1)))
-
-(defn mk-circle [x y r] {:c (Point. x y) :r r :links [] :id 0})
-(defn add-circle [c cs] 
-  (let [id (+ 1 (max (map :id cs)))]
-    (conj (assoc c :id id) cs)))
-
-(defn exclude
-  "Return c1 updated such that the overlap with c2 eliminted"
-  ([c1 c2] (exclude c1 c2 1.0))
-  ([c1 c2 fac]
-   (assoc c1
-          :c (set-dist (:c c1) (:c c2)
-                       (+ (:r c1) (:r c2)) ; desired-dist
-                       <
-                       fac))))
-
-(defn fix-dist 
-  "Return updated c1 such that the distance to center of c2 is fixed"
-  ([c1 c2] (fix-dist c1 c2 1.0))
-  ([c1 c2 fac] (fix-dist c1 c2 fac (+ (:r c1) (:r c2))))
-  ([c1 c2 fac dst]
-   (assoc c1
-          :c (set-dist (:c c1) (:c c2)
-                       dst ; desired-dist
-                       (constantly true)
-                       fac))))
-
-(defn keep-within [c1 c2] "Return updated c1 such that it stays within c2"
-  (assoc c1
-         :c (set-dist (:c c1) (:c c2)
-                      (- (:r c2) (:r c1)) ; desired-dist
-                      >
-                      1.0)))
 
 (defn foo
   "I don't do a whole lot."
